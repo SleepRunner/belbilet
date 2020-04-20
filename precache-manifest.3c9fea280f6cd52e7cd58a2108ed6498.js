8e0fdae5f304e11e6e3ba21218a8189b1b47855f
NightDreamer <vrazumi@mail.ru>
2020-04-20 11:16:24 +0300

    Updates

---
+++ precache-manifest.3c9fea280f6cd52e7cd58a2108ed6498.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de457741e11ff19e2b3dcb371ad26940",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b5b0bc028534c64043e",
+    "url": "/static/css/2.9e627a5a.chunk.css"
+  },
+  {
+    "revision": "90930cc85e846069a8bc",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "8b5b0bc028534c64043e",
+    "url": "/static/js/2.debfb509.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.debfb509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90930cc85e846069a8bc",
+    "url": "/static/js/main.eacdcd67.chunk.js"
+  },
+  {
+    "revision": "38cf3241f2ef2015fbd8",
+    "url": "/static/js/runtime-main.dde6ca1d.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
